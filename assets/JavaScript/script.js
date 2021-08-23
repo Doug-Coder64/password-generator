61ce3056a06549c04fd1b50f0e8ce1911be84d90
Doug Peterson <douglaspeterson6@gmail.com>
2021-08-23 18:38:45 -0500

    Added prompt to ask user for desired password length. TThat value is stored in generatePassword() for future use

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Generates password after other functions collect password criteria
+function generatePassword() {
+ const passwordLength = passwordLengthCheck();
+ console.log(passwordLength);
+ return true;
+}
+
+// Adds prompt to ask length of password and checks to see if it matches 8 - 128
+function passwordLengthCheck() {
+ const length = parseInt(window.prompt("Please Enter Desired Password Length"));
+ 
+  if (length >= 8 && length <= 128) {
+    return length;
+  }else {
+    window.alert("Password Length is invalid, please try again.");
+    return passwordLengthCheck();
+  }
+}
+
