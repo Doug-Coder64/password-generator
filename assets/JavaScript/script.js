cc2a6612a6bf653431cf6fe5216527477769a406
Doug Peterson <douglaspeterson6@gmail.com>
2021-08-30 18:32:29 -0500

    Fixed missing special characters

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -7,7 +7,6 @@ const uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numericCharacters = "0123456789";
 const specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-
 // Write password to the #password input
 function writePassword() {
   const password = generatePassword();
